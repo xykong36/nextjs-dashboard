aa877e8c3737c86e0d3b0dbc57a417a797f6948d
XIANGYU KONG <xiangyukongdeveloper@gmail.com>
2023-11-25 17:52:20 -0500

    create an invoice done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -90,7 +90,7 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
