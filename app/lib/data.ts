8ba4f978d1521502861ce0d3828ec488f35f5a0a
XIANGYU KONG <xiangyukongdeveloper@gmail.com>
2023-11-25 17:33:02 -0500

    extract the data from formData done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -90,7 +90,7 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 6;
+const ITEMS_PER_PAGE = 1;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,9 +5,9 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col px-8 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="h-30 mb-3 flex items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
